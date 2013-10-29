980daf6d0d05d5ed6b17ce8a99fe689a5f8eb9ac
NHIEUc <calvin.nhieu@gmail.com>
2013-10-29 13:15:42 -0400

    Added keyboard input function

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+var car;
+
 function update() {
-    
+    keyInput();
 }
 
 function draw() {
     
 }
 
+function keyInput () {
+	document.addEventListener('keydown', function(event) {
+    	if(event.keyCode == 37) { // left
+     		
+    	}
+    	else if(event.keyCode == 39) { // right
+        	
+    	}
+	});
+}
+
 $(document).ready(function() {
+	car = document.getElementById("car");
     setInterval(function() {
         update();
         draw();
