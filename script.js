03bf7a28bac918193677df886a58fdb802ae44e7
NHIEUc <calvin.nhieu@gmail.com>
2013-10-29 21:00:17 -0400

    Implemented duo-image road scrolling (with image clipping)

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ var battery; //test battery instance
 var fuel; //test fuel instance
 var itemSize; //size of pick-up items
 var trigger; //boolean for road animation
+var road1; //road 1 (test)
+var road2; //road 2 (test)
+
 
 Car = function() { //Car object
     this.x = 240; //xpos
@@ -20,20 +23,27 @@ Car = function() { //Car object
     this.xspeed = 250; //xspeed
     this.yspeed = 3; //yspeed
     img = new Image; //car image
-    img.src = "car.png"; //car source
+    img.src = "car.png"; //car image source
 };
 
-BatteryItem = function (x,y) { //Battery pick-up item object
+Road = function(y,src) { //Road object
+    this.x = 0; //x-position
+    this.y = y; //y-position
+    this.img = new Image; //road image
+    this.img.src = src; //road image source
+};
+
+BatteryItem = function (x,y) { //Battery pick-up item object (possibly combine with FuelItem...)
     this.x = x; //x-position
     this.y = y; //y-position
     this.color = 'orange'; //color of item
-}
+};
 
-FuelItem = function (x,y) { //Fuel pick-up item object
+FuelItem = function (x,y) { //Fuel pick-up item object (possibly combine with BatteryItem...)
     this.x = x; //x-position
     this.y = y; //y-position
     this.color = 'blue'; //color of item
-}
+};
 
 
 function moveCar () { //update car position
@@ -51,39 +61,46 @@ function moveCar () { //update car position
     }
 }
 
-function updateBattery () {
-    spawnInstances();
-    battery.y+=car.yspeed;
-    if (battery.y > c.height) battery = null;
-    spawnInstances();
+function updateRoad () { //update road position
+    road1.y+=car.yspeed; //move road down
+    if(road1.y >= c.height) road1.y=-c.height; //reset road to top
+    road2.y+=car.yspeed; //move road down
+    if(road2.y >= c.height) road2.y=-c.height; //reset road to top
+}
+
+function updateBattery () { //move battery items (to be modified)
+    spawnInstances(); //check if respawn required
+    battery.y+=car.yspeed; //move
+    if (battery.y > c.height) battery = null; //delete from memory
+    spawnInstances(); //check if respawn required
     if (battery.y+itemSize>=car.y && battery.y<car.y+car.height && battery.x+itemSize>=car.x && battery.x<=car.x+car.width) 
-        battery = null;
-    spawnInstances();
+        battery = null; //collision detection (delete from memory)
+    spawnInstances(); //check if respawn required
 }
 
-function updateFuel () {
-    spawnInstances();
-    fuel.y+=car.yspeed;
-    if (fuel.y > c.height) fuel = null;
-    spawnInstances();
+function updateFuel () { //move fuel items (to be modified)
+    spawnInstances(); //check if respawn required
+    fuel.y+=car.yspeed; //move
+    if (fuel.y > c.height) fuel = null; //delete from memory
+    spawnInstances(); //check if respawn required
     if (fuel.y+itemSize>=car.y && fuel.y<car.y+car.height && fuel.x+itemSize>=car.x && fuel.x<=car.x+car.width) 
-        fuel = null;
-    spawnInstances();
+        fuel = null; //collision detection (delete from memory)
+    spawnInstances(); //check if respawn required
 }
 
-function spawnBattery () {
-    if (battery == null) {
-        battery = new BatteryItem (Math.random()*(c.width-itemSize),Math.random()*(350));
+function spawnBattery () { //spawn new battery (to be obsolete)
+    if (battery == null) { //if none... 
+        battery = new BatteryItem (Math.random()*(c.width-itemSize),Math.random()*(350));//...create
     }
 }
 
-function spawnFuel () {
-    if (fuel == null) {
-        fuel = new FuelItem (Math.random()*(c.width-itemSize),Math.random()*(350));
+function spawnFuel () { //spawn new fuel (to be obsolete)
+    if (fuel == null) { //if none...
+        fuel = new FuelItem (Math.random()*(c.width-itemSize),Math.random()*(350)); //...create
     }
 }
 
-function setDelta () {
+function setDelta () { //calculate time (ms) between frames
     current = Date.now(); //ms at current time
     delta = (current-previous)/1000; //seconds since last frame
     previous = Date.now(); //ms at current time
@@ -118,6 +135,7 @@ function updateInstances () {
     if (trigger) { // if car is in static position
         updateBattery(); //update battery item position
         updateFuel(); //update fuel item position
+        updateRoad(); //update road scrolling
     }
 }
 
@@ -128,8 +146,8 @@ function update() { //update method
 }
 
 function draw() { //draw method
-    ctx.fillStyle = "#222"; //background color
-    ctx.fillRect(0,0,500,700); //redraw background
+    ctx.drawImage(road1.img,0,0,c.width,(c.height-road1.y),road1.x,road1.y,c.width,(c.height-road1.y)); //draw & clip road
+    ctx.drawImage(road2.img,0,0,c.width,(c.height-road2.y),road2.x,road2.y,c.width,(c.height-road2.y)); //draw & clip road
     ctx.drawImage(img,car.x,car.y); //draw car image
     ctx.fillStyle = battery.color; // set battery item color
     ctx.fillRect(battery.x,battery.y,itemSize,itemSize); // draw battery
@@ -146,10 +164,13 @@ $(document).ready(function() { //document loaded
     trigger = false; //for road animation
 
     c = document.getElementById("canvas"); //init canvas var
-    car = new Car(); //create car
     c.width=500; //set canvas size
     c.height=700; //set canvas height
     ctx=c.getContext("2d"); //init context (to draw)
+
+    car = new Car(); //create car
+    road1 = new Road (0,"RoadTest1.png"); //road image 1
+    road2 = new Road (-c.height,"RoadTest2.png"); //road image 2
 
     spawnInstances();
     setInterval(function() { //game loop
