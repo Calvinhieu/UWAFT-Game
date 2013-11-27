e922f44238a1ef75195b19a1950677cd681ac618
Calvinhieu <calvin.nhieu@gmail.com>
2013-11-27 12:48:52 -0500

    Added car meters. Edited images.

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ var left; //left key boolean
 var right; //right key boolean
 var battery; //test battery instance
 var fuel; //test fuel instance
+var fuelDisplay; //fuel display meter
+var batteryDisplay; //battery display meter
 var itemSize; //size of pick-up items
 var trigger; //boolean for road animation
 var road1; //road 1 (test)
 var road2; //road 2 (test)
+var lose; //lose boolean
+var batterySpawnTime; //for battery spawn rate
+var fuelSpawnTime; //for fuel spawn rate
+var minSpawnInterval; //minimum time between spawn intervals
 
 
 Car = function() { //Car object
@@ -21,9 +27,17 @@ Car = function() { //Car object
     this.width = 60; //width
     this.height = 100; //height
     this.xspeed = 250; //xspeed
-    this.yspeed = 3; //yspeed
-    img = new Image; //car image
-    img.src = "car.png"; //car image source
+    this.yspeed = 200; //yspeed
+    this.fuelMax = 100; //maximum fuel value
+    this.batteryMax = 100; //maximum battery value
+    this.fuel = this.fuelMax; //fuel
+    this.battery = this.batteryMax; //battery
+    this.fuelConsumptionRate = 0.05; //car's fuel consumption rate per frame
+    this.batteryConsumptionRate = 0.08; //car's battery consumption rate per frame
+    this.batteryRechargeRate = 0.05; //car's battery recharge rate per frame
+    this.batteryInUse = true; //is car using battery
+    this.img = new Image; //car image
+    this.img.src = "MinCar.png"; //car image source
 };
 
 Road = function(y,src) { //Road object
@@ -33,71 +47,110 @@ Road = function(y,src) { //Road object
     this.img.src = src; //road image source
 };
 
-BatteryItem = function (x,y) { //Battery pick-up item object (possibly combine with FuelItem...)
+PickUpItem = function (x,y,type,src) { //Batteries and Fuel items
     this.x = x; //x-position
     this.y = y; //y-position
-    this.color = 'orange'; //color of item
-};
+    this.img = new Image; //fuel/battery image
+    this.img.src = src;
+    if (type == 'fuel') { 
+        this.value = 10; //fuel recharges by 10
+        this.color = 'orange'; //represented by orange
+    }
+    else if (type == 'battery') { 
+        this.value = 15; //battery recharges by 15
+        this.color = 'blue'; //represented by blue
+    }
+}
 
-FuelItem = function (x,y) { //Fuel pick-up item object (possibly combine with BatteryItem...)
+DisplayMeter = function (x,y,type) { //top and right meters
     this.x = x; //x-position
     this.y = y; //y-position
-    this.color = 'blue'; //color of item
-};
-
+    this.width = 10; //bar width
+    this.border = 1; //bar border
+    this.borderColor = 'white'; //border color
+    if (type == 'fuel') {
+        this.height = car.fuelMax;
+        this.color = 'orange'; //orange represents fuel
+    }
+    else if (type == 'battery') {
+        this.height = car.batteryMax;
+        this.color = 'blue'; //blue represents battery
+    }
+}
 
 function moveCar () { //update car position
-    if(car.x >= 0) {
+    if(car.x >= 158) {
         if(left) car.x-=car.xspeed*delta; //move left
     }
-    if(car.x <= 500-car.width) {
+    if(car.x <= 336-car.width) {
         if (right) car.x+=car.xspeed*delta; //move right
     }
     if (car.y > 500) {
-        car.y-=car.yspeed; //update car's vertical
+        car.y-=car.yspeed*delta; //update car's vertical
     }
     else {
         trigger = true; //car is in static position
     }
 }
 
+function updateCarLevels () { //update fuel and battery levels of car
+    if (car.fuel <= 0 && car.battery <= 0) lose = true; //check for gameover
+   
+    if (car.batteryInUse) {
+        if (car.battery > 0) car.battery-=car.batteryConsumptionRate; // use battery
+    }
+
+    if (!car.batteryInUse) {
+        if (car.fuel > 0) car.fuel-=car.fuelConsumptionRate; // use fuel 
+        car.battery+=car.batteryRechargeRate; // charge battery
+    }
+
+    if (car.battery <= car.batteryMax*0.2) car.batteryInUse = false; //if car battery is at lower limit (20%), stop using battery
+    else if (car.battery >= car.batteryMax*0.25) car.batteryInUse = true; //if car battery is at upper limit (25%), start using battery
+}
+
 function updateRoad () { //update road position
-    road1.y+=car.yspeed; //move road down
+    road1.y+=car.yspeed*delta; //move road down
     if(road1.y >= c.height) road1.y=-c.height; //reset road to top
-    road2.y+=car.yspeed; //move road down
+    road2.y+=car.yspeed*delta; //move road down
     if(road2.y >= c.height) road2.y=-c.height; //reset road to top
 }
 
 function updateBattery () { //move battery items (to be modified)
-    spawnInstances(); //check if respawn required
-    battery.y+=car.yspeed; //move
-    if (battery.y > c.height) battery = null; //delete from memory
-    spawnInstances(); //check if respawn required
-    if (battery.y+itemSize>=car.y && battery.y<car.y+car.height && battery.x+itemSize>=car.x && battery.x<=car.x+car.width) 
-        battery = null; //collision detection (delete from memory)
-    spawnInstances(); //check if respawn required
+    if (battery) {
+        battery.y+=car.yspeed*delta; //move
+        if (battery.y > c.height) battery = null; //delete from memory
+        if (battery.y+itemSize>=car.y && battery.y<car.y+car.height && battery.x+itemSize>=car.x && battery.x<=car.x+car.width) {
+            car.battery+=battery.value;
+            if (car.battery > car.batteryMax) car.battery=car.batteryMax;
+            battery = null; //collision detection (delete from memory)
+        }
+    }
+    else {
+        if (Math.random() < 0.01) spawnInstances();
+    }
 }
 
 function updateFuel () { //move fuel items (to be modified)
-    spawnInstances(); //check if respawn required
-    fuel.y+=car.yspeed; //move
-    if (fuel.y > c.height) fuel = null; //delete from memory
-    spawnInstances(); //check if respawn required
-    if (fuel.y+itemSize>=car.y && fuel.y<car.y+car.height && fuel.x+itemSize>=car.x && fuel.x<=car.x+car.width) 
-        fuel = null; //collision detection (delete from memory)
-    spawnInstances(); //check if respawn required
+    if (fuel) {
+        fuel.y+=car.yspeed*delta; //move
+        if (fuel.y > c.height) fuel = null; //delete from memory
+        if (fuel.y+itemSize>=car.y && fuel.y<car.y+car.height && fuel.x+itemSize>=car.x && fuel.x<=car.x+car.width) {
+            car.fuel += fuel.value;
+            if (car.fuel > car.fuelMax) car.fuel=car.fuelMax;
+            fuel = null; //collision detection (delete from memory)
+        }
+    }
 }
 
 function spawnBattery () { //spawn new battery (to be obsolete)
-    if (battery == null) { //if none... 
-        battery = new BatteryItem (Math.random()*(c.width-itemSize),Math.random()*(350));//...create
-    }
+    if (!battery) //if none... 
+        battery = new PickUpItem (Math.random()*(158-itemSize)+184,Math.random()*(c.height/2),'battery','Battery.png');//...create
 }
 
 function spawnFuel () { //spawn new fuel (to be obsolete)
-    if (fuel == null) { //if none...
-        fuel = new FuelItem (Math.random()*(c.width-itemSize),Math.random()*(350)); //...create
-    }
+    if (!fuel) //if none...
+        fuel = new PickUpItem (Math.random()*(158-itemSize)+184,Math.random()*(c.height/2),'fuel','Fuel.png'); //...create
 }
 
 function setDelta () { //calculate time (ms) between frames
@@ -108,10 +161,10 @@ function setDelta () { //calculate time (ms) between frames
 
 function keyInput () { //keyboard input function
     document.addEventListener('keydown', function(event) {//add listener (KEYDOWN)
-        if(event.keyCode == 37) { // left
+        if(event.keyCode == 37 && !left) { // left
             left = true;
         }
-        else if(event.keyCode == 39) { // right
+        else if(event.keyCode == 39 && !right) { // right
             right = true;
         }
     });
@@ -136,31 +189,42 @@ function updateInstances () {
         updateBattery(); //update battery item position
         updateFuel(); //update fuel item position
         updateRoad(); //update road scrolling
+        updateCarLevels(); //update car values
     }
 }
 
 function update() { //update method
-    keyInput(); //check for keyboard input
-    setDelta(); //evaluate time (ms) from last frame
-    updateInstances(); //update entities positions (car, items)
+    if (!lose) {
+        keyInput(); //check for keyboard input
+        setDelta(); //evaluate time (ms) from last frame
+        updateInstances(); //update entities positions (car, items)
+    }
 }
 
 function draw() { //draw method
     ctx.drawImage(road1.img,0,0,c.width,(c.height-road1.y),road1.x,road1.y,c.width,(c.height-road1.y)); //draw & clip road
     ctx.drawImage(road2.img,0,0,c.width,(c.height-road2.y),road2.x,road2.y,c.width,(c.height-road2.y)); //draw & clip road
-    ctx.drawImage(img,car.x,car.y); //draw car image
-    ctx.fillStyle = battery.color; // set battery item color
-    ctx.fillRect(battery.x,battery.y,itemSize,itemSize); // draw battery
-    ctx.fillStyle = fuel.color; // set fuel color
-    ctx.fillRect(fuel.x,fuel.y,itemSize,itemSize); // draw fuel
+    ctx.drawImage(car.img,car.x,car.y); //draw car image
+    if (battery) ctx.drawImage(battery.img,battery.x,battery.y); // draw battery
+    if (fuel) ctx.drawImage(fuel.img,fuel.x,fuel.y); //draw fuel
+
+    // draw fuel and battery meters
+    ctx.fillStyle = fuelDisplay.borderColor;
+    ctx.fillRect (batteryDisplay.x-batteryDisplay.border,batteryDisplay.y-batteryDisplay.border,batteryDisplay.width+batteryDisplay.border*2,batteryDisplay.height+batteryDisplay.border*2);
+    ctx.fillRect (fuelDisplay.x-fuelDisplay.border,fuelDisplay.y-fuelDisplay.border,fuelDisplay.width+fuelDisplay.border*2,fuelDisplay.height+fuelDisplay.border*2);
+    ctx.fillStyle = batteryDisplay.color;
+    ctx.fillRect (batteryDisplay.x,batteryDisplay.y+(batteryDisplay.height-car.battery),batteryDisplay.width,car.battery);
+    ctx.fillStyle = fuelDisplay.color;
+    ctx.fillRect (fuelDisplay.x,fuelDisplay.y+(fuelDisplay.height-car.fuel),fuelDisplay.width,car.fuel);
 }
 
 $(document).ready(function() { //document loaded
 	FPS=60; //set to 60fr/sec
+    lose = false; //not lose
     left = false; //left boolean to false
     right = false; //right boolean to true
-    previous = 0; //initially set "time since last frame" to 0
-    itemSize = 10; //pick-up item size
+    previous = Date.now(); //initially set "time since last frame" to 0
+    itemSize = 20; //pick-up item size
     trigger = false; //for road animation
 
     c = document.getElementById("canvas"); //init canvas var
@@ -169,12 +233,17 @@ $(document).ready(function() { //document loaded
     ctx=c.getContext("2d"); //init context (to draw)
 
     car = new Car(); //create car
-    road1 = new Road (0,"RoadTest1.png"); //road image 1
-    road2 = new Road (-c.height,"RoadTest2.png"); //road image 2
+    road1 = new Road (0,"MinRoad.png"); //road image 1
+    road2 = new Road (-c.height,"MinRoad.png"); //road image 2
+    fuelDisplay = new DisplayMeter (30,10,'fuel'); //inst display meter for car's fuel level
+    batteryDisplay = new DisplayMeter (10,10,'battery'); //inst display meter for car's battery level
 
     spawnInstances();
-    setInterval(function() { //game loop
-        update(); //update 
-        draw(); //draw
-    }, 1000/FPS); //set FPS to 60fps
+    loop();
 });
+
+function loop () {
+    requestAnimationFrame(loop);
+    update(); //update 
+    draw(); //draw
+}
